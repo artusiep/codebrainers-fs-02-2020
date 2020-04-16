243b5f6c430f48a3962e3be4847e9802e09d9f1c
Artur Siepietowski <artursiepietowski@gmail.com>
2020-04-16 21:22:29 +0200

    Second Classes

---
+++ oop/day2/mask.py
@@ -0,0 +1,12 @@
+germs = ["bacteria", "viruses", "fungus"]
+filtered_germs = filter(lambda x: x != "viruses", germs)
+
+print()
+
+# Lazy Evaluation
+
+if True:
+    exit()
+
+print(list(filtered_germs))
+
